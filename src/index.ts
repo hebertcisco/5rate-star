59ea430500cacb60f0eebddd680175914e5697ba
hebertcisco <hebertcisco@outlook.com>
2022-05-13 11:47:40 +0000

    v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { StarModule } from './resources/star/star.module';
+import { Rating, SumRating } from './shared/core';
 
+export { StarModule } from './resources/star/star.module';
 export { Rating, SumRating } from './shared/core';
 export type { ISumRating, ISumRatingItem, RatingType } from './shared/core';
-export default StarModule;
+export default {
+  StarModule,
+  Rating,
+  SumRating,
+};
